b4466f4478ed58772a9a14d7b998d36c9199d8b2
Andrea Bartolucci <andreaio.it@hotmail.it>
2014-09-04 14:08:29 +0200

    Fixed several issues

--- downloader.py
+++ downloader.py
@@ -1,25 +1,40 @@
+import errno
+import json
+import os
 import requests
 import threading
-import json
 
 class ChapterDownloader(threading.Thread):
     def __init__(self, chapterId, chapterName, path):
+        super(ChapterDownloader, self).__init__()
         self.chapterId = chapterId
         self.chapterName = chapterName
         self.path = path
 
-    def run():
-        getImageList()
-        downloadImages()
-        print "Chapter " + chapterName + " Done\n"
+    def run(self):
+        self.getImageList()
+        self.downloadImages()
+        print "Chapter " + self.chapterName + " Done\n"
 
     def getImageList(self):
-        self.imageList = requests.get("http://www.mangaeden.com/api/chapter/" + chapterId)
+        self.imageList = requests.get("http://www.mangaeden.com/api/chapter/" + self.chapterId)
         self.imageList = json.loads(self.imageList.text);
         self.imageList = self.imageList["images"]
 
     def downloadImages(self):
-        for img in imageList:
+        for img in self.imageList:
             image = requests.get("http://cdn.mangaeden.com/mangasimg/" + img[1])
-            with open(path+"/"+chapterName+"/"+img[0]+".png") as file:
-                file.write(image.text)
+            finalpath = os.path.join(self.path, self.chapterName)
+            #create the directory
+            try:
+                os.makedirs(finalpath)
+            except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                    raise
+            #start writing
+            with open(os.path.join(finalpath, str(img[0])+".jpg"), "wb") as file:
+                file.write(image.text.encode("UTF-8"))
+
+foo = ChapterDownloader("5064306fc092253769022d52", "36", "/home/andrea/mangaedenDownloads")
+foo.start()
+foo.join()
